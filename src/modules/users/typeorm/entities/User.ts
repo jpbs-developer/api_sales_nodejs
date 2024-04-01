3cb8c1ac11e55514bfbe6d40ab80baa77d38e134
jpbs-developer <jpbs1@aluno.ifal.edu.br>
2024-04-01 19:58:22 -0300

    feat: creating user entity

---
+++ src/modules/users/typeorm/entities/User.ts
@@ -0,0 +1,33 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+export default class UserEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({
+    nullable: true,
+  })
+  avatar: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
