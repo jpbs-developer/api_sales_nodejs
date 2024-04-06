81d4ec652110e43dda310bd094f7b6e8044d16e9
jpbs-developer <jpbs1@aluno.ifal.edu.br>
2024-04-06 10:16:00 -0300

    feat: overriding express's Request method

--- src/middlewares/isAuthenticated.ts
+++ src/middlewares/isAuthenticated.ts
@@ -3,6 +3,12 @@ import AppError from '@shared/errors/AppError';
 import { NextFunction, Request, Response } from 'express';
 import { verify } from 'jsonwebtoken';
 
+interface JwtPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
 export default function isAuthenticated(
   request: Request,
   response: Response,
@@ -16,7 +22,13 @@ export default function isAuthenticated(
   const [_, token] = authHeader.split(' ');
   const { JWT_HASH } = process.env;
   try {
-    verify(token, String(JWT_HASH));
+    const decodedToken = verify(token, String(JWT_HASH)) as JwtPayload;
+    const { sub } = decodedToken;
+
+    request.user = {
+      userId: sub,
+    };
+
     return next();
   } catch (error) {
     throw new AppError('Invalid JWT token');
