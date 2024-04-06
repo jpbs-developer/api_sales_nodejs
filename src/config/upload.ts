7512a4935dd80d761c5c4e36d5b4a996006a6420
jpbs-developer <jpbs1@aluno.ifal.edu.br>
2024-04-06 10:24:33 -0300

    feat: configuring multer

---
+++ src/config/upload.ts
@@ -0,0 +1,15 @@
+import multer from 'multer';
+import path from 'path';
+
+const uploadFolder = path.resolve(__dirname, '..', '..', 'uploads');
+
+export default {
+  directory: uploadFolder,
+  storage: multer.diskStorage({
+    destination: uploadFolder,
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+      cb(null, file.fieldname + '-' + uniqueSuffix);
+    },
+  }),
+};
